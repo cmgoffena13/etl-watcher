667c34f26f217d1948987059a070bec92d4b9190
Cortland Goffena <cortland.goffena@hotmail.com>
2025-10-04 21:13:28 -0600

    uppercase project

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "watcher"
+project = "Watcher"
 copyright = "2025, Cortland Goffena"
 author = "Cortland Goffena"
 release = "1.0.0"
